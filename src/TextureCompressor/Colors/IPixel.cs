6084c1bce0daa8a73f17ced35c8897d11bc4f495
迎风听雨 <1259546233@qq.com>
2026-05-28 15:03:48 +0800

    给颜色转换提供默认接口实现

--- src/TextureCompressor/Colors/IPixel.cs
+++ src/TextureCompressor/Colors/IPixel.cs
@@ -3,50 +3,67 @@ namespace TextureCompressor.Colors;
 public interface IPixel<TSelf>
     where TSelf : unmanaged, IPixel<TSelf>
 {
-    static virtual Rgba8UNorm ToRgba8UNorm(TSelf value) => throw CreateUnsupportedConversionException(nameof(ToRgba8UNorm));
+    static abstract Rgba8UNorm ToRgba8UNorm(TSelf value);
 
-    static virtual TSelf FromRgba8UNorm(Rgba8UNorm value) => throw CreateUnsupportedConversionException(nameof(FromRgba8UNorm));
+    static abstract TSelf FromRgba8UNorm(Rgba8UNorm value);
 
-    static virtual Rgba8SNorm ToRgba8SNorm(TSelf value) => throw CreateUnsupportedConversionException(nameof(ToRgba8SNorm));
+    static virtual Rgba8SNorm ToRgba8SNorm(TSelf value) =>
+        Rgba8UNorm.ToRgba8SNorm(TSelf.ToRgba8UNorm(value));
 
-    static virtual TSelf FromRgba8SNorm(Rgba8SNorm value) => throw CreateUnsupportedConversionException(nameof(FromRgba8SNorm));
+    static virtual TSelf FromRgba8SNorm(Rgba8SNorm value) =>
+        TSelf.FromRgba8UNorm(Rgba8UNorm.FromRgba8SNorm(value));
 
-    static virtual Rgba16UNorm ToRgba16UNorm(TSelf value) => throw CreateUnsupportedConversionException(nameof(ToRgba16UNorm));
+    static virtual Rgba16UNorm ToRgba16UNorm(TSelf value) =>
+        Rgba8UNorm.ToRgba16UNorm(TSelf.ToRgba8UNorm(value));
 
-    static virtual TSelf FromRgba16UNorm(Rgba16UNorm value) => throw CreateUnsupportedConversionException(nameof(FromRgba16UNorm));
+    static virtual TSelf FromRgba16UNorm(Rgba16UNorm value) =>
+        TSelf.FromRgba8UNorm(Rgba8UNorm.FromRgba16UNorm(value));
 
-    static virtual Rgba16SNorm ToRgba16SNorm(TSelf value) => throw CreateUnsupportedConversionException(nameof(ToRgba16SNorm));
+    static virtual Rgba16SNorm ToRgba16SNorm(TSelf value) =>
+        Rgba8UNorm.ToRgba16SNorm(TSelf.ToRgba8UNorm(value));
 
-    static virtual TSelf FromRgba16SNorm(Rgba16SNorm value) => throw CreateUnsupportedConversionException(nameof(FromRgba16SNorm));
+    static virtual TSelf FromRgba16SNorm(Rgba16SNorm value) =>
+        TSelf.FromRgba8UNorm(Rgba8UNorm.FromRgba16SNorm(value));
 
-    static virtual Rgba32UNorm ToRgba32UNorm(TSelf value) => throw CreateUnsupportedConversionException(nameof(ToRgba32UNorm));
+    static virtual Rgba32UNorm ToRgba32UNorm(TSelf value) =>
+        Rgba8UNorm.ToRgba32UNorm(TSelf.ToRgba8UNorm(value));
 
-    static virtual TSelf FromRgba32UNorm(Rgba32UNorm value) => throw CreateUnsupportedConversionException(nameof(FromRgba32UNorm));
+    static virtual TSelf FromRgba32UNorm(Rgba32UNorm value) =>
+        TSelf.FromRgba8UNorm(Rgba8UNorm.FromRgba32UNorm(value));
 
-    static virtual Rgba32SNorm ToRgba32SNorm(TSelf value) => throw CreateUnsupportedConversionException(nameof(ToRgba32SNorm));
+    static virtual Rgba32SNorm ToRgba32SNorm(TSelf value) =>
+        Rgba8UNorm.ToRgba32SNorm(TSelf.ToRgba8UNorm(value));
 
-    static virtual TSelf FromRgba32SNorm(Rgba32SNorm value) => throw CreateUnsupportedConversionException(nameof(FromRgba32SNorm));
+    static virtual TSelf FromRgba32SNorm(Rgba32SNorm value) =>
+        TSelf.FromRgba8UNorm(Rgba8UNorm.FromRgba32SNorm(value));
 
-    static virtual Rgba16Float ToRgba16Float(TSelf value) => throw CreateUnsupportedConversionException(nameof(ToRgba16Float));
+    static virtual Rgba16Float ToRgba16Float(TSelf value) =>
+        Rgba8UNorm.ToRgba16Float(TSelf.ToRgba8UNorm(value));
 
-    static virtual TSelf FromRgba16Float(Rgba16Float value) => throw CreateUnsupportedConversionException(nameof(FromRgba16Float));
+    static virtual TSelf FromRgba16Float(Rgba16Float value) =>
+        TSelf.FromRgba8UNorm(Rgba8UNorm.FromRgba16Float(value));
 
-    static virtual Rgba32Float ToRgba32Float(TSelf value) => throw CreateUnsupportedConversionException(nameof(ToRgba32Float));
+    static virtual Rgba32Float ToRgba32Float(TSelf value) =>
+        Rgba8UNorm.ToRgba32Float(TSelf.ToRgba8UNorm(value));
 
-    static virtual TSelf FromRgba32Float(Rgba32Float value) => throw CreateUnsupportedConversionException(nameof(FromRgba32Float));
+    static virtual TSelf FromRgba32Float(Rgba32Float value) =>
+        TSelf.FromRgba8UNorm(Rgba8UNorm.FromRgba32Float(value));
 
-    static virtual Rgba64UNorm ToRgba64UNorm(TSelf value) => throw CreateUnsupportedConversionException(nameof(ToRgba64UNorm));
+    static virtual Rgba64UNorm ToRgba64UNorm(TSelf value) =>
+        Rgba8UNorm.ToRgba64UNorm(TSelf.ToRgba8UNorm(value));
 
-    static virtual TSelf FromRgba64UNorm(Rgba64UNorm value) => throw CreateUnsupportedConversionException(nameof(FromRgba64UNorm));
+    static virtual TSelf FromRgba64UNorm(Rgba64UNorm value) =>
+        TSelf.FromRgba8UNorm(Rgba8UNorm.FromRgba64UNorm(value));
 
-    static virtual Rgba64SNorm ToRgba64SNorm(TSelf value) => throw CreateUnsupportedConversionException(nameof(ToRgba64SNorm));
+    static virtual Rgba64SNorm ToRgba64SNorm(TSelf value) =>
+        Rgba8UNorm.ToRgba64SNorm(TSelf.ToRgba8UNorm(value));
 
-    static virtual TSelf FromRgba64SNorm(Rgba64SNorm value) => throw CreateUnsupportedConversionException(nameof(FromRgba64SNorm));
+    static virtual TSelf FromRgba64SNorm(Rgba64SNorm value) =>
+        TSelf.FromRgba8UNorm(Rgba8UNorm.FromRgba64SNorm(value));
 
-    static virtual Rgba64Float ToRgba64Float(TSelf value) => throw CreateUnsupportedConversionException(nameof(ToRgba64Float));
+    static virtual Rgba64Float ToRgba64Float(TSelf value) =>
+        Rgba8UNorm.ToRgba64Float(TSelf.ToRgba8UNorm(value));
 
-    static virtual TSelf FromRgba64Float(Rgba64Float value) => throw CreateUnsupportedConversionException(nameof(FromRgba64Float));
-
-    private static NotSupportedException CreateUnsupportedConversionException(string methodName) =>
-        new($"Pixel type '{typeof(TSelf).Name}' does not support conversion '{methodName}'.");
+    static virtual TSelf FromRgba64Float(Rgba64Float value) =>
+        TSelf.FromRgba8UNorm(Rgba8UNorm.FromRgba64Float(value));
 }
